e0661cb49caf065470b70866f3016a2e62b00ce6
Julien Grobshäußer <julien.grobshaeusser@gmail.com>
2023-09-14 00:15:15 +0200

    Implemented ShouldBroadcast interface

--- app/Events/UpdateProfileInformation.php
+++ app/Events/UpdateProfileInformation.php
@@ -11,7 +11,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class UpdateProfileInformation
+class UpdateProfileInformation implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
